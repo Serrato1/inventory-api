93d46861110b5e644245c5b467703fb7d1bf3bb8
Noel Serrato <Serrato@LGMBP.local>
2019-05-05 22:20:55 -0700

    access control allow credentials is true

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8000;
 
-var routes = require('./config/routes');
-routes(app);
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials",true)
     next();
 });
+
+var routes = require('./config/routes');
+routes(app);
+
+
   
 app.listen(port,() => {
     console.log('Server Listening on Port', port);
